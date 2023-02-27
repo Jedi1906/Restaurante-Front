917810aaab3e1c3f8a6f5acf945544d34eb71e9c
Jedi1906 <jedidiah.hernandez@xiaexperience.com>
2023-02-27 08:30:41 -0600

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./views/home/home.component";
+import {CarComponent} from "./views/car/car.component";
+import {SoldComponent} from "./views/sold/sold.component";
+import {TicketsComponent} from "./views/tickets/tickets.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent, data:{title: 'Comidas a tus horas'}},
+  {path: 'pedidos', component: CarComponent, data: {title: 'Realiza tu pedido aqui'}},
+  {path: 'ventas', component: SoldComponent, data: {title: 'Carrito de compras'}},
+  {path: 'comprobantes', component: TicketsComponent, data: {title: 'Tus comprobantes'}},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, CarComponent, SoldComponent, TicketsComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './headers/sidebar/sidebar.component';
+import { HomeComponent } from './views/home/home.component';
+import { CarComponent } from './views/car/car.component';
+import { SoldComponent } from './views/sold/sold.component';
+import { TicketsComponent } from './views/tickets/tickets.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    CarComponent,
+    SoldComponent,
+    TicketsComponent, routingComponents, CardComponent
   ],
   imports: [
     BrowserModule,
