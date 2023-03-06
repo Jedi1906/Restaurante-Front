f047505c2fe259943bac8cdf4b1f7b62767101e3
paolaHernandez23 <paolahernandezhernandez058@gmail.com>
2023-03-06 08:22:12 -0600

    correcciones

--- src/app/views/logins/login/login.component.ts
+++ src/app/views/logins/login/login.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private router: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  checkurl(){
+    if (this.router.url.indexOf("login") != -1){
+      return false;
+    } else if (this.router.url.indexOf("dashboard")!= -1){
+      return false;
+    }else {
+      return true;
+    }
   }
 
 }
